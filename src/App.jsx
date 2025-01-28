1681ee5c72e23ec783f594ef383daa44b9d866c6
ishika <jainishika072@gmail.com>
2025-01-28 12:20:32 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
 import Navbar from './comp/navbar'
+import Footer from './comp/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
        <Navbar></Navbar>
+       <Footer></Footer>
     </>
   )
 }
