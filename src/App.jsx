ed90a1e00185d2ec3ef582e30bdbc9cb1f392fd4
ishika <jainishika072@gmail.com>
2025-01-28 13:57:16 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Button } from './components/ui/button'
 import Navbar from './comp/navbar'
 import Footer from './comp/Footer'
 import HeroSection from './comp/HeroSection'
+import About from './comp/About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +15,7 @@ function App() {
     <>
        <Navbar></Navbar>
        <HeroSection></HeroSection>
-      
+          <About></About>
        <Footer></Footer>
     </>
   )
